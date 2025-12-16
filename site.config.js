e1ea32741cd455a329ec94aee3185391877418b5
7-8th-floor <126578425+7-8th-floor@users.noreply.github.com>
2025-12-16 22:15:21 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "시발",
+    name: "7.8 floor",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "시발아",
-    bio: "아이시발",
+    role: "",
+    bio: "",
     email: "",
     linkedin: "",
     github: "",
@@ -18,15 +18,15 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "7.8th-floor",
-    description: "",
-    scheme: "light", // 'light' | 'dark' | 'system'
+    title: "morethan-log",
+    description: "welcome to morethan-log!",
+    scheme: "dark", // 'light' | 'dark' | 'system'
   },
 
   // CONFIG configration (required)
   link: "https://morethan-log.vercel.app",
-  since: , // If leave this empty, current year will be used.
-  lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  since: 2022, // If leave this empty, current year will be used.
+  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
@@ -73,4 +73,3 @@ const CONFIG = {
 }
 
 module.exports = { CONFIG }
-
